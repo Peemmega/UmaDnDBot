3ab7d3dbfee227bc1a1de5a57a5315eb93b414d0
Peemmega <Peemmegachan@gmail.com>
2026-04-21 18:44:44 +0700

    add Fujimasa March

--- cogs/game.py
+++ cogs/game.py
@@ -264,6 +264,7 @@ class GameCog(commands.GroupCog, name="game"):
         discord.app_commands.Choice(name="Rookie Pace", value="rookie_pace"),
         discord.app_commands.Choice(name="Rookie Late", value="rookie_late"),
         discord.app_commands.Choice(name="Rookie End", value="rookie_end"),
+        discord.app_commands.Choice(name="Fujimasa March", value="fujimasa_march"),
         discord.app_commands.Choice(name="Field Pace", value="runner_pace"),
         discord.app_commands.Choice(name="Oguri Cap", value="chaser_late"),
         discord.app_commands.Choice(name="Obey Your Master", value="sprinter_end"),
