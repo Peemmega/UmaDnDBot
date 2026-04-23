d94782527455978def4636a8a08ca5d7cb89979f
Peemmega <Peemmegachan@gmail.com>
2026-04-23 22:51:40 +0700

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from utils.database import get_player
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://umabotapp-production-c99a.up.railway.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/player/{user_id}")
 def api_get_player(user_id: int):
