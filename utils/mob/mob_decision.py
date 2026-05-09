6f67af5fc143f2e217d71d380052fd1b1d60b206
Peemmega <Peemmegachan@gmail.com>
2026-05-09 20:32:12 +0700

    Update mob_decision.py

--- utils/mob/mob_decision.py
+++ utils/mob/mob_decision.py
@@ -54,11 +54,14 @@ def estimate_rule_value(rule: dict) -> float:
     # selected = d if kh is None else min(d, kh)
 
     # d เยอะ + kh เยอะ = สกิล roll scaling คุ้มขึ้นมาก
-    value = (d ** 2) * 16
+    value = 0
     # value += selected * 28
 
     if kh is not None:
         value = (kh ** 2) * 16
+        value += (d) * 16
+    else:
+        value = (d ** 2) * 16
 
     return value
 
