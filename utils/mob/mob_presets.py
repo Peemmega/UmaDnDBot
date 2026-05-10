e417cb8a22dbbe60fa40fdaa8ff2d984a0c1cdbd
Peemmega <Peemmegachan@gmail.com>
2026-05-10 16:48:33 +0700

    Update mob_presets.py

--- utils/mob/mob_presets.py
+++ utils/mob/mob_presets.py
@@ -73,9 +73,9 @@ MOB_PRESETS = {
         "race_profile": {
             "speed": 4,
             "stamina": 4,
-            "power": 5,
+            "power": 6,
             "gut": 2,
-            "wit": 2,
+            "wit": 1,
 
             "turf": 1,"dirt": 1,
             "sprint": 1,"mile": 1,"medium": 1,"long": 1,
