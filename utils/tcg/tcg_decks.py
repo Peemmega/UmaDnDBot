0df6553ebc3624c1e7f59915f2e93eb8f70f6886
Peemmega <Peemmegachan@gmail.com>
2026-05-17 16:01:25 +0700

    update deck

--- utils/tcg/tcg_decks.py
+++ utils/tcg/tcg_decks.py
@@ -508,6 +508,37 @@ CARD_DATABASE = {
   }
 }
 
+CARD_DATABASE.update({
+  "UMBT01-01": {"id": "UMBT01-01", "name": "Sakura Laurel", "type": "Trainee", "cost": 6, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_01.webp", "text": "TODO"},
+  "UMBT01-02": {"id": "UMBT01-02", "name": "Sakura Bakushin O", "type": "Trainee", "cost": 6, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_02.webp", "text": "TODO"},
+  "UMBT01-03": {"id": "UMBT01-03", "name": "Sakura Chiyono O", "type": "Trainee", "cost": 4, "power": 3000, "image": "/tcg/cards/trainees/UMBT01_03.webp", "text": "TODO"},
+  "UMBT01-04": {"id": "UMBT01-04", "name": "Sakura Chitose O", "type": "Trainee", "cost": 4, "power": 3000, "image": "/tcg/cards/trainees/UMBT01_04.webp", "text": "TODO"},
+  "UMBT01-05": {"id": "UMBT01-05", "name": "Haru Urara", "type": "Trainee", "cost": 3, "power": 2000, "image": "/tcg/cards/trainees/UMBT01_05.webp", "text": "TODO"},
+  "UMBT01-06": {"id": "UMBT01-06", "name": "The Path to Spring", "type": "Event", "cost": 0, "power": 0, "image": "/tcg/cards/trainees/UMBT01_06.webp", "text": "TODO"},
+  "UMBT01-07": {"id": "UMBT01-07", "name": "Fierce Battle Dodgeball", "type": "Event", "cost": 2, "power": 0, "image": "/tcg/cards/trainees/UMBT01_07.webp", "text": "TODO"},
+  "UMBT01-08": {"id": "UMBT01-08", "name": "The Waltz of Spring", "type": "Event", "cost": 4, "power": 0, "image": "/tcg/cards/trainees/UMBT01_08.webp", "text": "TODO"},
+  "UMBT01-09": {"id": "UMBT01-09", "name": "Almond Eye", "type": "Trainee", "cost": 7, "power": 5000, "image": "/tcg/cards/trainees/UMBT01_09.webp", "text": "TODO"},
+  "UMBT01-10": {"id": "UMBT01-10", "name": "Still in love", "type": "Trainee", "cost": 6, "power": 6000, "image": "/tcg/cards/trainees/UMBT01_10.webp", "text": "TODO"},
+  "UMBT01-11": {"id": "UMBT01-11", "name": "Daring Tact", "type": "Trainee", "cost": 4, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_11.webp", "text": "TODO"},
+  "UMBT01-12": {"id": "UMBT01-12", "name": "Catch my rhythm", "type": "Event", "cost": 2, "power": 0, "image": "/tcg/cards/trainees/UMBT01_12.webp", "text": "TODO"},
+  "UMBT01-13": {"id": "UMBT01-13", "name": "Love me Love me Love me", "type": "Event", "cost": 3, "power": 0, "image": "/tcg/cards/trainees/UMBT01_13.webp", "text": "TODO"},
+  "UMBT01-14": {"id": "UMBT01-14", "name": "Vixena", "type": "Trainee", "cost": 5, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_14.webp", "text": "TODO"},
+  "UMBT01-15": {"id": "UMBT01-15", "name": "Cheval Grand", "type": "Trainee", "cost": 4, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_15.webp", "text": "TODO"},
+  "UMBT01-16": {"id": "UMBT01-16", "name": "Vivlos", "type": "Trainee", "cost": 3, "power": 2000, "image": "/tcg/cards/trainees/UMBT01_16.webp", "text": "TODO"},
+  "UMBT01-17": {"id": "UMBT01-17", "name": "Uma Summer Time Yay~", "type": "Event", "cost": 3, "power": 0, "image": "/tcg/cards/trainees/UMBT01_17.webp", "text": "TODO"},
+  "UMBT01-18": {"id": "UMBT01-18", "name": "Beyond the Horizon, We Meet", "type": "Event", "cost": 3, "power": 0, "image": "/tcg/cards/trainees/UMBT01_18.webp", "text": "TODO"},
+  "UMBT01-19": {"id": "UMBT01-19", "name": "Admire Vega", "type": "Trainee", "cost": 6, "power": 5000, "image": "/tcg/cards/trainees/UMBT01_19.webp", "text": "TODO"},
+  "UMBT01-20": {"id": "UMBT01-20", "name": "Sister", "type": "Trainee", "cost": 4, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_20.webp", "text": "TODO"},
+  "UMBT01-21": {"id": "UMBT01-21", "name": "It is fun to running with those two, Is not it?", "type": "Event", "cost": 3, "power": 0, "image": "/tcg/cards/trainees/UMBT01_21.webp", "text": "TODO"},
+  "UMBT01-22": {"id": "UMBT01-22", "name": "Togerther, with you", "type": "Event", "cost": 2, "power": 0, "image": "/tcg/cards/trainees/UMBT01_22.webp", "text": "TODO"},
+  "UMBT01-23": {"id": "UMBT01-23", "name": "Curren Chan", "type": "Trainee", "cost": 5, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_23.webp", "text": "TODO"},
+  "UMBT01-24": {"id": "UMBT01-24", "name": "Smart Falcon", "type": "Trainee", "cost": 5, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_24.webp", "text": "TODO"},
+  "UMBT01-25": {"id": "UMBT01-25", "name": "Gold City", "type": "Trainee", "cost": 5, "power": 4000, "image": "/tcg/cards/trainees/UMBT01_25.webp", "text": "TODO"},
+  "UMBT01-26": {"id": "UMBT01-26", "name": "Grand Live!", "type": "Event", "cost": 1, "power": 0, "image": "/tcg/cards/trainees/UMBT01_26.webp", "text": "TODO"},
+  "UMBT01-27": {"id": "UMBT01-27", "name": "Fire dance festival", "type": "Event", "cost": 8, "power": 0, "image": "/tcg/cards/trainees/UMBT01_27.webp", "text": "TODO"},
+  "UMT-006": {"id": "UMT-006", "name": "Trainer Tsubaki", "type": "Trainer", "cost": 5, "power": 5000, "image": "/tcg/cards/trainers/UMT_006.webp", "text": "TODO"},
+})
+
 
 def get_card(card_id: str) -> dict | None:
     return CARD_DATABASE.get(card_id)
@@ -728,7 +759,122 @@ STARTER_DECKS = [
     }
   },
 ]
-PREDEFINED_DECKS = [build_deck(deck) for deck in STARTER_DECKS]
+
+CUSTOM_DECKS = [
+  {
+    "id": "sakura-laurel",
+    "name": "Sakura Laurel Deck",
+    "description": "Sakura lineup built around Laurel and UMTD04 support.",
+    "style": "Speed",
+    "highlight": "Sakura Laurel leads the tempo package.",
+    "tags": ["Custom", "Sakura", "Tempo"],
+    "trainer": "UMT-006",
+    "mainDeck": {
+      "UMBT01-01": 4,
+      "UMBT01-03": 4,
+      "UMBT01-02": 2,
+      "UMTD04-02": 4,
+      "UMTD04-05": 4,
+      "UMBT01-04": 4,
+      "UMBT01-06": 3,
+      "UMTD04-08": 4,
+      "UMTD04-07": 4,
+      "UMBT01-08": 4,
+      "UMTD04-10": 3,
+    },
+  },
+  {
+    "id": "v-family",
+    "name": "V Family Deck",
+    "description": "V family core backed by Opera O and Meisho Doto.",
+    "style": "Stamina",
+    "highlight": "Vixena, Cheval Grand, and Vivlos anchor the deck.",
+    "tags": ["Custom", "V Family", "Stamina"],
+    "trainer": "UMT-003",
+    "mainDeck": {
+      "UMBT01-14": 4,
+      "UMBT01-15": 4,
+      "UMBT01-16": 4,
+      "UMBT01-17": 4,
+      "UMBT01-18": 4,
+      "UMTD02-01": 4,
+      "UMTD02-03": 4,
+      "UMTD02-07": 2,
+      "UMTD02-08": 4,
+      "UMTD02-09": 2,
+      "UMTD02-10": 4,
+    },
+  },
+  {
+    "id": "tiara",
+    "name": "Tiara Deck",
+    "description": "Almond Eye package mixed with the UMTD01 Tiara suite.",
+    "style": "Speed",
+    "highlight": "Almond Eye and Oguri Cap share the top end.",
+    "tags": ["Custom", "Tiara", "Hybrid"],
+    "trainer": "UMT-002",
+    "mainDeck": {
+      "UMBT01-09": 4,
+      "UMTD01-02": 2,
+      "UMBT01-11": 4,
+      "UMTD01-03": 4,
+      "UMTD01-04": 4,
+      "UMTD01-01": 2,
+      "UMTD01-05": 2,
+      "UMBT01-12": 2,
+      "UMTD01-08": 2,
+      "UMBT01-13": 2,
+      "UMTD01-07": 4,
+      "UMTD01-09": 4,
+      "UMTD01-10": 4,
+    },
+  },
+  {
+    "id": "admire-vega",
+    "name": "Admire Vega Deck",
+    "description": "Admire Vega booster cards with UMTD03 power support.",
+    "style": "Power",
+    "highlight": "Admire Vega drives a compact 40-card pressure plan.",
+    "tags": ["Custom", "Admire Vega", "Power"],
+    "trainer": "UMT-004",
+    "mainDeck": {
+      "UMBT01-19": 4,
+      "UMTD03-02": 4,
+      "UMBT01-20": 4,
+      "UMBT01-21": 4,
+      "UMBT01-22": 4,
+      "UMTD03-04": 4,
+      "UMTD03-05": 4,
+      "UMTD03-08": 4,
+      "UMTD03-09": 4,
+      "UMTD03-10": 4,
+    },
+  },
+  {
+    "id": "still-in-love",
+    "name": "Still in love Deck",
+    "description": "Still in love list with Daring Tact and UMTD01 events.",
+    "style": "Guts",
+    "highlight": "Still in love leads a lean event-heavy shell.",
+    "tags": ["Custom", "Still in love", "Events"],
+    "trainer": "UMT-002",
+    "mainDeck": {
+      "UMBT01-10": 4,
+      "UMTD01-02": 2,
+      "UMBT01-11": 4,
+      "UMTD01-03": 4,
+      "UMTD01-04": 4,
+      "UMBT01-12": 3,
+      "UMTD01-08": 4,
+      "UMBT01-13": 3,
+      "UMTD01-07": 4,
+      "UMTD01-09": 4,
+      "UMTD01-10": 4,
+    },
+  },
+]
+
+PREDEFINED_DECKS = [build_deck(deck) for deck in [*STARTER_DECKS, *CUSTOM_DECKS]]
 DECKS_BY_ID = {deck['id']: deck for deck in PREDEFINED_DECKS}
 
 
