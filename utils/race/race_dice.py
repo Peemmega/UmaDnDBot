5fe017a8fbc5d4d6accb364292ca2912d02b6e4e
Peemmega <Peemmegachan@gmail.com>
2026-06-15 23:40:18 +0700

    rebalance

--- utils/race/race_dice.py
+++ utils/race/race_dice.py
@@ -146,20 +146,18 @@ def roll_by_rule(rule: dict, player_stats: dict, game_player: dict, context: dic
     # Roll Dice --------------------------------------
     modified_selected = []
 
-    spd_multiplier = path_effect.get("spd_multiplier", 1.0)
     power_total_multiplier = path_effect.get("power_total_multiplier", 1.0)
 
-    spd_bonus_raw = player_stats.get("speed", 1)
-    speed_Bonus = int(spd_bonus_raw * spd_multiplier)  * 3
-
-    power_bonus = player_stats.get("power", 1) * 2
-    stamina_bonus = player_stats.get("stamina", 1) * 3
+    speed_Bonus = player_stats.get("speed", 1) * 2
+    power_bonus = player_stats.get("power", 1) * power_total_multiplier
+    stamina_bonus = player_stats.get("stamina", 1) * 1
 
     nearby_count = min(context.get("nearby_count", 0), 2)
-    gut_scale = player_stats.get("gut", 1) * 2
-    gut_bonus = (gut_scale * nearby_count ) if context.get("distance_color") == "Gold" else 0
+    gut_scale = player_stats.get("gut", 1) 
+    gut_bonus = 0
 
     if (context.get("phase", 1) >= 3):
+        gut_bonus = (gut_scale * nearby_count ) if context.get("distance_color") == "Gold" else 0
         gut_bonus = math.floor(gut_bonus * 2)
 
     total_selected_die_bonus = 0
